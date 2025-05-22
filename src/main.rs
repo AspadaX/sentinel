1d4c07e7e639cb8cd580b3d07786b6055fb0d06e
Xinyu Bao <34500975+AspadaX@users.noreply.github.com>
2025-05-22 23:08:31 +0800

    initial upload

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+/*
+    This program intends to do the following:
+    - Check whether a given code file suits the specified business logics
+    - If it does not, the checker will report errors
+*/
+fn main() {
+    println!("Hello, world!");
+}
